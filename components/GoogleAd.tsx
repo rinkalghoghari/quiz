c4e7d2ce7cdc1d6210e34e397470fb8a80f0bfae
RinkalGhoghari <rg.hmrtech@gmail.com>
2025-11-25 16:02:06 +0530

    updated googlead componets

--- components/GoogleAd.tsx
+++ components/GoogleAd.tsx
@@ -148,7 +148,7 @@ const GoogleAd: React.FC<GoogleAdProps> = ({
 
   // Production ad
   return (
-    <div className={`google-ad-container ${className}`} style={style}>
+    <div className={`google-ad-container ${className}`} style={{ width: "100%", minWidth: "320px", ...style }}>
       <ins
         ref={adRef}
         className="adsbygoogle"
