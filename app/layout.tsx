0394d35e75b2ffc0f3f3ab79a1c3ee5802f32067
RinkalGhoghari <rg.hmrtech@gmail.com>
2025-11-25 16:27:29 +0530

    solve console error1

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,38 +33,7 @@ export default function RootLayout({
           name="google-adsense-account"
           content="ca-pub-5504771682915102"
         />
-        {/* Google Funding Choices (Consent Dialog) */}
-        <Script
-          id="google-fc-present"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function() {
-                function signalGooglefcPresent() {
-                  if (!window.frames['googlefcPresent']) {
-                    if (document.body) {
-                      const iframe = document.createElement('iframe');
-                      iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;';
-                      iframe.style.display = 'none';
-                      iframe.name = 'googlefcPresent';
-                      document.body.appendChild(iframe);
-                    } else {
-                      setTimeout(signalGooglefcPresent, 0);
-                    }
-                  }
-                }
-                signalGooglefcPresent();
-              })();
-            `
-          }}
-        />
-
-        <Script
-          id="google-fc-script"
-          strategy="afterInteractive"
-          src="https://fundingchoicesmessages.google.com/i/pub-YOUR_PUBLISHER_ID?ers=1"
-          async
-        />
+   
       </head>
 
       <body
